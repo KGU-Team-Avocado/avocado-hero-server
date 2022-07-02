f5e5815032284bf2509d0f268ba226269a35091d
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-07-02 09:41:50 +0900

    로딩 메시지 수정

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json({limit: "50mb"}));
 let ip = require("ip");
 console.dir('현재 사용중인 내부 ip...');
 console.log ('ip : \'http://'+ip.address()+'\',' );
-console.dir('위 한 줄을 복사해서 config.js의 ip 줄에 그대로 덮어쓰기 하세요.');
-console.dir('위의 ip가 맞지 않는 경우 cmd에서 ipconfig를 통해 ip를 확인하세요.');
+// console.dir('위 한 줄을 복사해서 config.js의 ip 줄에 그대로 덮어쓰기 하세요.');
+// console.dir('위의 ip가 맞지 않는 경우 cmd에서 ipconfig를 통해 ip를 확인하세요.');
 console.log();
 
 mongoose.connect(`mongodb+srv://soyoung:qnstksalcqudfufcjfl@cluster0.c7eeq.mongodb.net/avocado_hero?authSource=admin&replicaSet=atlas-rkqtpg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, {})
