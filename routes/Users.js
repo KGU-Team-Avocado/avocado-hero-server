2412376ea2f7d8d0910aa3ee41674ad50072ac5d
seeun01 <ssky601@gmail.com>
2022-07-07 14:49:23 +0900

    로그인 기능 구현중

--- routes/Users.js
+++ routes/Users.js
@@ -26,4 +26,32 @@ router.post("/register", (req, res) => {
   });
 });
 
+router.post("/login", (req, res) => {
+  // post 요청을 처리해 응답을 주는 함수가 들어올 위치.
+  const account = req.body;
+  User.findOne(
+    {
+      $and: [{ user_id: account.id }, { user_password: account.password }],
+    },
+    function (error, user) {
+      if (error) {
+        console.log(error);
+        return res.json({ status: "error", user: user });
+      } else {
+        console.log(user);
+        if (user === null) {
+          return res.json({ status: "fail", user: user });
+        } else {
+          const securedUser = {
+            user_id: user.user_id,
+            user_name: user.user_name,
+            user_email: user.user_email,
+          };
+          return res.json({ status: "success", user: securedUser });
+        }
+      }
+    }
+  );
+});
+
 module.exports = router;
