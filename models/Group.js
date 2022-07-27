7a022cd628006ad1cbae8b1f207c3647bc54eb70
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-27 17:17:58 +0900

    그룹 등록용 스키마와 파일 추가중

---
+++ models/Group.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+// 자주 쓰는 데이터 타입
+const groupSchema = mongoose.Schema({
+    // _id는 새로운 데이터가 생성될 때 mongodb에서 자동으로 만들어주는 기본키임. 스키마를 생성할 때 따로 작성하지 않아도 됨.
+    test_id: {
+        type: String,
+        maxlength: 30,
+        unique: 1,  // 기본키와 같은 역할을 해줌
+        required: true  // not null
+    },
+    test_num: {
+        type: Number,
+        default: 0
+    },
+    test_date: {
+        type: Date
+    },
+    test_bool: {
+        type: Boolean,
+        default: false
+    },
+    test_list: [
+        String
+    ],
+    test_json: {
+        one: String,
+        two: Number,
+        three: Boolean
+    },
+    test_json_list: [{
+        date: String,
+        value: String,
+        isDone: Boolean,
+        key: String
+    }],
+})
+
+const Group = mongoose.model('Group', groupSchema)
+
+module.exports = { Group }
