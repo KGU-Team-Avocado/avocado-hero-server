0f09cc66bad10800ac612d308f5818a877e8361c
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-09-02 23:56:24 +0900

    북마크 버튼 수정중

--- routes/Bookmarks.js
+++ routes/Bookmarks.js
@@ -8,7 +8,6 @@ const { Bookmark } = require("../models/Bookmark");
 router.post("/bookmarkSave", function(req, res) {
     console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
     // 데이터 저장
-    
     Bookmark.updateOne(
         {user_id:req.body.user_id},
         {$push: {company_id:req.body.company_id}},
@@ -24,6 +23,29 @@ router.post("/bookmarkSave", function(req, res) {
     });
 });
 
+router.post("/bookmarkDelete", function(req, res, next) {
+    console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
+    // 데이터 삭제
+
+    Bookmark.updateOne(
+        { user_id:req.body.user_id },
+        {
+          $pull: {company_id:req.body.company_id}
+        }).exec((error, notices) => {
+          if (error) {
+            console.log(error);
+            return res.json({ status: 'error', error })
+          } else {
+            Bookmark.findOne({ user_id:req.body.user_id }).then((Bookmark) => {
+              res.json(Bookmark.notices)
+            }).catch((err) => {
+              console.log(err);
+              next(err)
+            });
+          }
+        });
+});
+
 router.post("/getMyBookmark", (req, res, next) => {
     console.log(req.body);
     Bookmark.findOne({ user_id: req.body.user_id }).then((bookmarks) => {
@@ -36,5 +58,6 @@ router.post("/getMyBookmark", (req, res, next) => {
         next(err)
     });
 });
+
 
 module.exports = router;
