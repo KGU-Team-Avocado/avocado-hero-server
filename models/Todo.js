374bbc8d64cef2122dd49bbf86b2d184ab387dfc
seeun01 <ssky601@gmail.com>
2022-09-07 19:27:14 +0900

    투두 db에 프로젝트 id 저장하도록 변경

--- models/Todo.js
+++ models/Todo.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const todoSchema = mongoose.Schema({
-    todo_id: {
-        type: String,
-        maxlength: 30,
-    },
+    // todo_id: {
+    //     type: String,
+    //     maxlength: 30,
+    // },
     todo_text: {
         type: String,
         maxlength: 100,
     },
     todo_isChecked: {
         type: Boolean,
+    },
+    project_id: {
+        type: String,
     }
 })
 
