64f0cb7faebb6ab055fed2d7583af70f1f7c1d12
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-26 00:54:32 +0900

    북마크 오류 핸들링

--- routes/Bookmarks.js
+++ routes/Bookmarks.js
@@ -5,57 +5,62 @@ const { Company } = require("../models/Company");
 const { User } = require("../models/User");
 const { Bookmark } = require("../models/Bookmark");
 
-router.post("/bookmarkSave", function(req, res) {
-    console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
-    // 데이터 저장
-    Bookmark.updateOne(
-        {user_id:req.body.user_id},
-        {$push: {company_id:req.body.company_id}},
-        {upsert:true},
-        function(error, data){
-        if(error){
-            console.log(error);
-            return res.json({status: 'false', error}) 
-        }else{
-            console.log('Saved!')
-            return res.json({status: 'success'})
-        }
+router.post("/bookmarkSave", function (req, res) {
+  console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
+  // 데이터 저장
+  Bookmark.updateOne(
+    { user_id: req.body.user_id },
+    { $push: { company_id: req.body.company_id } },
+    { upsert: true },
+    function (error, data) {
+      if (error) {
+        console.log(error);
+        return res.json({ status: 'false', error })
+      } else {
+        console.log('Saved!')
+        return res.json({ status: 'success' })
+      }
     });
 });
 
-router.post("/bookmarkDelete", function(req, res, next) {
-    console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
-    // 데이터 삭제
+router.post("/bookmarkDelete", function (req, res, next) {
+  console.log(req.body); // 프론트에서 스키마 형식에 맞는 데이터 넘겨줌
+  // 데이터 삭제
 
-    Bookmark.updateOne(
-        { user_id:req.body.user_id },
-        {
-          $pull: {company_id:req.body.company_id}
-        }).exec((error, notices) => {
-          if (error) {
-            console.log(error);
-            return res.json({ status: 'error', error })
-          } else {
-            Bookmark.findOne({ user_id:req.body.user_id }).then((Bookmark) => {
-              res.json(Bookmark.notices)
-            }).catch((err) => {
-              console.log(err);
-              next(err)
-            });
-          }
+  Bookmark.updateOne(
+    { user_id: req.body.user_id },
+    {
+      $pull: { company_id: req.body.company_id }
+    }).exec((error, notices) => {
+      if (error) {
+        console.log(error);
+        return res.json({ status: 'error', error })
+      } else {
+        Bookmark.findOne({ user_id: req.body.user_id }).then((Bookmark) => {
+          res.json(Bookmark.notices)
+        }).catch((err) => {
+          console.log(err);
+          next(err)
         });
+      }
+    });
 });
 
 router.post("/getMyBookmark", (req, res, next) => {
-    console.log(req.body);
-    Bookmark.findOne({ user_id: req.body.user_id }).then((bookmarks) => {
-        console.log(bookmarks.company_id);
-        Company.find({'_id': { $in: bookmarks.company_id }}).then((companies) => {
-            res.json(companies);
+  console.log(req.body);
+  Bookmark.findOne({ user_id: req.body.user_id })
+    .then((bookmarks) => {
+      if (bookmarks !== null) {
+        Company.find({ '_id': { $in: bookmarks.company_id } }).then((companies) => {
+          res.json(companies);
         })
+      }
+      else {
+        res.json([]);
+      }
     }).catch((err) => {
-        console.log(err);
-        next(err)
+      console.log(err);
+      next(err)
     });
 });
 
