dc9739a138fe7c722ec4e20dbd87fa689d1f5d2a
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-10 22:47:06 +0900

    feat: add Organization schema

---
+++ models/Organization.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+// 자주 쓰는 데이터 타입
+const organizationSchema = mongoose.Schema({
+    // _id는 새로운 데이터가 생성될 때 mongodb에서 자동으로 만들어주는 기본키임. 스키마를 생성할 때 따로 작성하지 않아도 됨.
+    title: {
+        type: String,
+        required: true  // not null
+    },
+    code: {
+        type: String,
+        required: true  // not null
+    },
+    notice: {
+        type: String,
+        required: true  // not null
+    },
+    maxTeam: {
+        type: Number,
+        required: true  // not null
+    },
+    maxMember: {
+        type: Number,
+        required: true  // not null
+    },
+})
+
+const Organization = mongoose.model('Organization', organizationSchema)
+
+module.exports = { Organization }
