8a92200d6d83c2b315dcd811386f190f4b81d216
soyoung125 <sypark041754@gmail.com>
2022-11-17 20:45:33 +0900

    feat: 평가 통계를 위해 평가 데이터 스키마 변경

--- models/Evaluation.js
+++ models/Evaluation.js
@@ -14,6 +14,7 @@ const evaluationSchema = mongoose.Schema({
         required: true
     },
     score_eval: [{
+        label: String,
         questions: [{
             id: String,
             score: Number
